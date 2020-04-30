94d9453e8c5487612a592c0248f54709084b78b3
Jonathan Marquez <jonathanmarquezj@gmail.com>
2020-04-30 12:21:51 +0200

    Correccion de errores

--- app.py
+++ app.py
@@ -4,19 +4,24 @@ import json
 
 app = Flask(__name__)
 
+with open("MSX.json") as fichero:
+	lista_juegos_msx=json.load(fichero)
 
+#INICIO
 @app.route('/', methods=["GET", "POST"])
 def inicio():
-	return render_template("base.html")
+	return render_template("inicio.html")
 
+#PARA BUSCAR JUEGOS
 @app.route('/juegos', methods=["GET", "POST"])
 def juegos():
 	return render_template("juegos.html")
 
+#PARA LISTAR LOS JUEGOS ENCONTADOS
 @app.route('/listajuegos', methods=['GET', "POST"])
 def listajuegos():
 	if request.method == 'POST': #Si tiene POST
-		return render_template("listajuegos.html")
+		return render_template("listajuegos.html", lista_juegos_msx=lista_juegos_msx)
 	else: #En caso de que no tenga POST
 		abort(404)
 
